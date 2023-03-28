f844bc204fe19e849310ef98ba3a2a8ac31accbf
Jonatan Antoni <jonatan.antoni@arm.com>
2023-03-28 15:58:33 +0200

    Initial contribution

---
+++ main/footer.js
@@ -0,0 +1,7 @@
+function writeHeader() {
+    document.write('Version 0.0.0-dirty1');
+};
+
+function writeFooter()  {
+    document.write('Generated on Tue Mar 28 2023 13:40:49 for RTX RTOS 0.0.0-dirty1+g769c8bd. Copyright &copy; 2023 Arm Limited (or its affiliates). All rights reserved.');
+};
